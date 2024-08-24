52ad1f676223d4b6368feb884bce2c53ec349c44
BigTO <d22495521@gmail.com>
2024-08-24 20:30:49 +0800

    更新版拉最後了

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ var express = require("express");
 var router = express.Router();
 require("dotenv").config();
 
-const { ENV_VARIABLES, PORT } = process.env;
+const { ENV_VARIABLE, PORT } = process.env;
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "我是8778", ENV_VARIABLES });
+  res.render("index", { title: "我是8778", ENV_VARIABLE });
 });
 
 module.exports = router;
