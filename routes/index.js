2400e87b30af87048447313b81550e63bcc24f9e
BigTO <d22495521@gmail.com>
2024-08-24 20:27:12 +0800

    更新版喔

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const { ENV_VARIABLES, PORT } = process.env;
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "我是87", ENV_VARIABLES });
+  res.render("index", { title: "我是8778", ENV_VARIABLES });
 });
 
 module.exports = router;
